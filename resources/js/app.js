d296572a2070f923eec385dbb4747ae194709644
nellonlan <nellonlan@mail.ru>
2022-05-03 05:57:43 +0300

    CaptchaProd

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,9 @@ window.Vue = require('vue').default;
 import App from './components/App'
 import Posts from './components/pages/posts/Index'
 import ValidationErrors from './components/parts/validationErrors'
-import {VueRecaptcha} from 'vue-recaptcha';
 
 Vue.component('posts', Posts);
 Vue.component('validation-errors', ValidationErrors);
-Vue.component('vue-recaptcha', {VueRecaptcha});
 
 const app = new Vue({
     el: '#app',
